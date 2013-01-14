d777126559d9b1edb86c18f4bb3c6b9c11ed8bd2
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2013-01-14 17:11:16 +0900

    AddressView作成

--- js/addressbook.js
+++ js/addressbook.js
@@ -35,4 +35,37 @@ $(function(){
   });
   // インスタンスの生成
   var Addresses = new AddressCollection;
+
+  // ===================================================
+  // view class
+  // ===================================================
+  // AddressViewクラスを定義
+  var AddressView = Backbone.View.extend({
+    tagName: 'li',
+    className: 'address-item',
+    // イベントハンドラの設定
+    events: {
+      'dblclick label.name': 'rename',
+      'click button.delete': 'clear'
+    },
+    initialize: function(){
+      // モデルへのバインド
+      this.model.bind('change', this.render, this);
+      this.model.bind('destroy', this.remove, this);
+    },
+    render: function(){
+      $(this.el).html(
+        $('<label class="name">').text(this.model.get('name'))
+      ).append('<button class="delete">Delete</button>');
+      return this;
+    },
+    rename: function(){
+      var newName = window.prompt('Enter new name.', this.model.get('name'));
+      this.model.save('name', newName);
+    },
+    clear: function(){
+      this.model.destroy();
+    }
+  });
+
 })
