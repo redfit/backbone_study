e7dd439b551fbcb787f5ae1fd0a50a98410b4372
daisuke fuchiwaki <daisuke.fuchiwaki@excel.co.jp>
2013-01-14 16:44:45 +0900

    AddressModel作成

--- js/addressbook.js
+++ js/addressbook.js
@@ -1,2 +1,22 @@
 $(function(){
+  // Addressクラスを定義
+  var Address = Backbone.Model.extend({
+    // デフォルト値
+    defaults: {
+      name: ''
+    },
+    // 初期化
+    initialize: function() {
+      if (!this.get('name')) {
+        this.set({name: this.defaults.name});
+      }
+    },
+    // バリデーション
+    validate: function(attributes){
+      var name = attributes.name;
+      if(!name || name === this.defaults.name){
+        return 'Error!';
+      }
+    }
+  })
 })
